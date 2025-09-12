072d74d2f3a41d80d7e5474778e6653fe1190f88
MariamEssam <romaessam570@gmail>
2025-09-12 23:03:45 +0300

    add fields to bottom sheet

--- lib/views/widgets/add_note_button_sheet.dart
+++ lib/views/widgets/add_note_button_sheet.dart
@@ -6,15 +6,74 @@ class AddNoteButtonSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 28,
-          ),
-          CustomTextField()
-        ],
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    final keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
+
+    return Container(
+      height: screenHeight * 0.85,
+      child: Padding(
+        padding: EdgeInsets.only(
+          left: screenWidth * 0.04,
+          right: screenWidth * 0.04,
+          bottom: keyboardHeight + 16,
+          top: 16,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: screenWidth * 0.12,
+              height: 4,
+              margin: EdgeInsets.only(bottom: screenHeight * 0.03),
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(2),
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    CustomTextField(
+                      hint: "Title",
+                    ),
+                    SizedBox(
+                      height: screenHeight * 0.02,
+                    ),
+                    CustomTextField(
+                      hint: "Content",
+                      maxLines: screenHeight > 600 ? 6 : 3,
+                    ),
+                    // SizedBox(
+                    //   height: screenHeight * 0.03,
+                    // ),
+                    if (screenHeight > 500)
+                      Container(
+                        height: screenHeight * 0.25,
+                        width: double.infinity,
+                        child: Image.asset(
+                          "assets/images/abc.png",
+                          fit: BoxFit.contain,
+                          errorBuilder: (context, error, stackTrace) {
+                            return Container(
+                              height: screenHeight * 0.25,
+                              child: Icon(
+                                Icons.image_not_supported,
+                                size: screenWidth * 0.15,
+                                color: Colors.grey,
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
